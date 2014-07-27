54cd6a2ec3aad7581c98f275fee4b416e5371567
Robert Andrew Stevens <robertandrewstevens@gmail.com>
2014-07-27 16:48:48 -0500

    Ch. 1
    added

---
+++ SoDA01.R
@@ -0,0 +1,20 @@
+# Software for Data Analysis:  Programming with R
+# by John M. Chambers
+# Chapter 1:  Introduction: Principles and Concepts
+# 1.1 Exploration: The Mission
+# 1.2 Trustworthy Software: The Prime Directive
+# 1.3 Concepts for Programming with R
+# 1.4 The R System and the S Language
+
+library(SoDA)
+help(Examples)
+exampleFiles()
+#runExample(what, where, TRUE, TRUE)
+
+## 1.1 Exploration: The Mission
+
+## 1.2 Trustworthy Software: The Prime Directive
+
+## 1.3 Concepts for Programming with R
+
+## 1.4 The R System and the S Language
